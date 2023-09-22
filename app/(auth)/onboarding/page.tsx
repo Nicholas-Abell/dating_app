bbc77920700dfb98a83e55f16da4a6f407773742
Nick Abell <darkforceyoda@gmail.com>
2023-09-22 08:58:33 -0700

    AccountProfile com created, installed zod/react-hook-forms

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,11 +1,25 @@
+"use client";
+import AccountProfile from "@/components/navbar/forms/AccountProfile";
 import React from "react";
+import { currentUser } from "@clerk/nextjs";
 
 type pageProps = {};
 
 const page: React.FC<pageProps> = async () => {
+  const user = await currentUser();
+
+  //user clerk
+  //userInfo MongoDb
+  const userData = {
+    id: user?.id,
+    userName: userInfo?.username || user?.username,
+    
+  };
+
   return (
-    <main>
+    <main className="flex flex-col justify-center">
       <h1>ON BOARDING</h1>
+      <AccountProfile />
     </main>
   );
 };
