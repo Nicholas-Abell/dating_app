f07a097a50867ec77cfc25d72136dc261a1856ec
Nick Abell <darkforceyoda@gmail.com>
2023-09-24 15:24:07 -0700

    simple form, connected to MongoDB

--- components/navbar/forms/AccountProfile.tsx
+++ components/navbar/forms/AccountProfile.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { UserValidation } from "@/libs/validations/User";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
+import { updateUser } from "@/libs/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
 
 type AccountProfileProps = {
   user: {
@@ -14,39 +16,67 @@ type AccountProfileProps = {
 };
 
 const AccountProfile: React.FC<AccountProfileProps> = ({ user }) => {
+  const [profile, setProfile] = useState({
+    username: "",
+    bio: "",
+  });
+  const router = useRouter();
+  const pathname = usePathname();
+
   const form = useForm({
     resolver: zodResolver(UserValidation),
     defaultValues: {
-      id: "",
       username: "",
       bio: "",
     },
   });
 
-  const onSubmit = (values: z.infer<typeof UserValidation>) => {};
+  const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+    await updateUser({
+      userId: user.id,
+      username: profile.username,
+      bio: profile.bio,
+      path: pathname,
+    });
+
+    console.log("user updated");
+
+    if (pathname === "/profile/edit") {
+      router.back();
+    } else {
+      router.push("/");
+    }
+  };
 
   return (
-    <form className="flex flex-col gap-4 bg-slate-800 rounded-lg p-4 text-white">
-      <label>
-        Username:
-        <input
-          type="text"
-          name="username"
-          placeholder={user.username}
-          className="px-2 text-black ml-2"
-        />
-      </label>
-      <label>
-        Bio:
-        <textarea
-          name="bio"
-          placeholder={user.bio}
-          className="px-2 text-black ml-2"
-          rows={10}
-        />
-      </label>
-      <button type="submit">Edit Profile</button>
-    </form>
+    <>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-4 bg-slate-800 rounded-lg p-4 text-white"
+      >
+        <label>
+          Username:
+          <input
+            {...form.register("username")}
+            type="text"
+            name="username"
+            className="px-2 text-black ml-2"
+          />
+        </label>
+        <label>
+          Bio:
+          <textarea
+            {...form.register("bio")}
+            name="bio"
+            className="px-2 text-black ml-2"
+            rows={10}
+          />
+        </label>
+        <button type="submit">Edit Profile</button>
+      </form>
+      {profile.username}
+      {profile.bio}
+    </>
   );
 };
 export default AccountProfile;
