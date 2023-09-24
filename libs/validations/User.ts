a3fc068253690a1b67a0d95c2ca39d1e25782287
Nick Abell <darkforceyoda@gmail.com>
2023-09-24 15:22:35 -0700

    User From zod validation

--- libs/validations/User.ts
+++ libs/validations/User.ts
@@ -1,7 +1,7 @@
 import * as z from 'zod';
 
 export const UserValidation = z.object({
-    id: z.string(),
+    // id: z.string(),
     username: z.string().min(3).max(25),
     bio: z.string().max(1000),
 })
