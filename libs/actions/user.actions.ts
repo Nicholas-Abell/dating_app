d01fa41d8183d7bca5cb6bc6654e48945dc9ae98
Nick Abell <darkforceyoda@gmail.com>
2023-09-24 15:21:48 -0700

    next js server action for user profile -> MongoDb

--- libs/actions/user.actions.ts
+++ libs/actions/user.actions.ts
@@ -1,2 +1,32 @@
 "use server"
 
+import { revalidatePath } from "next/cache";
+import User from "../models/user.model";
+import { connectToDB } from "../mongoose"
+
+interface Params {
+    userId: string,
+    username: string,
+    bio: string,
+    path: string
+}
+
+export async function updateUser({userId, username, bio, path}: Params): Promise<void> {
+    connectToDB();
+
+    try {
+        await User.findOneAndUpdate(
+            {id: userId}, 
+            {username: username.toLowerCase(), bio, onboarded: true}, 
+            {upsert: true}
+            );
+    
+            if(path === '/profile/edit'){
+                revalidatePath(path)
+            }
+        
+    } catch (error: any) {
+        throw new Error(`Failed to create/update user: ${error.message}`)
+    }
+
+} 
