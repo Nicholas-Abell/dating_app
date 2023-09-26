3734f6148cccbcb090fd759e3dd4ef61ff398a9a
Nick Abell <darkforceyoda@gmail.com>
2023-09-26 08:12:53 -0700

    user navigation

---
+++ components/navbar/LeftSidebar.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import React from "react";
+import { BiHome, BiMessageSquareDots } from "react-icons/bi";
+import { ImProfile } from "react-icons/im";
+import { AiFillSetting } from "react-icons/ai";
+
+type LeftSidebarProps = {};
+
+const LeftSidebar: React.FC<LeftSidebarProps> = () => {
+  return (
+    <div className="hidden md:flex flex-col sticky items-start gap-12 h-screen px-4 pt-48 left-0 top-0 bg-black z-10 text-white font-bold">
+      <Link href="/" className="flex justify-center items-center gap-4 p-2">
+        <BiHome size={25} className="text-white" />
+        <p className="hidden lg:block">HOME</p>
+      </Link>
+      <Link
+        href="/onboarding"
+        className="flex justify-center items-center gap-4 p-2"
+      >
+        <ImProfile size={25} className="text-white" />
+        <p className="hidden lg:block">PROFILE</p>
+      </Link>
+      <div className="flex justify-center items-center gap-4 p-2">
+        <BiMessageSquareDots size={30} className="text-white" />
+        <p className="hidden lg:block">MESSAGES</p>
+      </div>
+      <div className="flex justify-center items-center gap-4 p-2">
+        <AiFillSetting size={30} className="text-white" />
+        <p className="hidden lg:block">HOME</p>
+      </div>
+    </div>
+  );
+};
+export default LeftSidebar;
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -5,8 +5,8 @@ type NavbarProps = {};
 
 const Navbar: React.FC<NavbarProps> = () => {
   return (
-    <div className="w-full bg-black px-8 py-2">
-      <UserButton afterSignOutUrl="/sign-in" />
+    <div className="w-full bg-black px-8 py-2 z-20 fixed top-0 left-0">
+      <UserButton afterSignOutUrl="/sign-in"/>
     </div>
   );
 };
