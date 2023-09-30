9c09528b5a37b6d62005bced8e5b08a0ebc10ccb
Nick Abell <darkforceyoda@gmail.com>
2023-09-30 09:23:29 -0700

    message form added to profiles

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { fetchUser } from "@/libs/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 import React from "react";
 
@@ -11,11 +12,21 @@ async function Page({ params }: { params: { id: string } }) {
   //   if (!userInfo?.onboarded) redirect("/onboarding");
 
   return (
-    <section className="h-screen w-full bg-green-800 flex flex-col justify-center items-center">
+    <section className="h-screen w-full relative flex flex-col justify-center items-center">
       <h1>{userInfo?.username}</h1>
       <h1>{userInfo?.age}</h1>
       <h1>{userInfo?.bio}</h1>
       <h1>{userInfo?.lookingfor}</h1>
+      <button>Message</button>
+      <div className="absolute w-full h-screen flex flex-col bottom-12 justify-end items-center">
+        <form className="w-full max-w-[380px]">
+          <input
+            type="text"
+            className="text-black p-4 placeholder:text-black w-full rounded-full"
+            placeholder="...say something"
+          />
+        </form>
+      </div>
     </section>
   );
 }
