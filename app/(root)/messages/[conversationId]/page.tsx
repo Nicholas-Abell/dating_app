93dc5a221f16c66f9a61bd96e7e469ed9b451b3e
Nick <darkforceyoda@gmail.com>
2023-10-08 21:18:37 -0500

    message styles

--- app/(root)/messages/[conversationId]/page.tsx
+++ app/(root)/messages/[conversationId]/page.tsx
@@ -1,18 +1,28 @@
 import { fetchConversation } from "@/libs/actions/message.actions";
+import { fetchUser } from "@/libs/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 
 async function Page({ params }: { params: { conversationId: string } }) {
-  // const user = await currentUser();
-  // if (!user) return null;
+  const user = await currentUser();
+  if (!user) return null;
+  const userInfo = await fetchUser(user?.id);
 
-  // const userInfo = await fetchConversation(user?.id);
   console.log("params:" + params.conversationId);
   const conversation = await fetchConversation(params?.conversationId);
 
   return (
-    <section className="h-screen w-full flex flex-col justify-center items-center pt-8">
+    <section className="h-screen w-full gap-4 flex flex-col justify-center items-center pt-8 lg:px-48">
       {conversation?.message.map((mess: any) => (
-        <p key={mess._id}>{mess.content}</p>
+        <div
+          className={`border-2 border-white rounded-xl px-12 py-4 ${
+            mess.sentBy === userInfo?.username
+              ? " bg-green-400 mr-auto"
+              : "bg-blue-400 ml-auto"
+          }`}
+          key={mess._id}
+        >
+          <p>{mess.content}</p>
+        </div>
       ))}
     </section>
   );
