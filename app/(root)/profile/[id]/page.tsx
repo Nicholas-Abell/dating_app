6465c2ec8e67eb0d3860eb3e568bc7ee00842993
Nick <darkforceyoda@gmail.com>
2023-10-09 10:04:07 -0500

    Edit Profile on user profile view, profile styles

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -2,7 +2,11 @@ import Message from "@/components/forms/Message";
 import { fetchUser } from "@/libs/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import React from "react";
-import { BsFillPersonFill } from "react-icons/bs";
+import { BsFillPersonFill, BsPersonHearts } from "react-icons/bs";
+import { BiRuler } from "react-icons/bi";
+import { FaTransgender } from "react-icons/fa";
+import { PiGlobeStandBold } from "react-icons/pi";
+import Link from "next/link";
 
 async function Page({ params }: { params: { id: string } }) {
   const user = await currentUser();
@@ -11,45 +15,73 @@ async function Page({ params }: { params: { id: string } }) {
   const userInfo = await fetchUser(user?.id);
   const profileInfo = await fetchUser(params.id);
   console.log(profileInfo);
-  //   if (!userInfo?.onboarded) redirect("/onboarding");
 
   return (
     <section className="w-full min-h-screen relative flex flex-col pt-12 pb-24 items-center gap-4">
       <div className="w-full h-[80vh] bg-emerald-800" />
-      <div className="w-full flex items-center pt-8 px-8 gap-12 text-4xl">
-        <h1 className="font-bold uppercase">{profileInfo?.username}</h1>
-        <h1>{profileInfo?.age}</h1>
-      </div>
-      <div className="w-full flex items-center px-8 gap-4">
-        <div className="flex items-center gap-1">
-          <div className="p-1 bg-green-400 rounded-full" />
-          <p>Online Now</p>
+      <div className="w-full min-h-screen relative flex flex-col pt-12 pb-24 px-8 items-center gap-4">
+        <div className="w-full flex items-center pt-8 gap-12 text-4xl">
+          <h1 className="font-bold uppercase">{profileInfo?.username}</h1>
+          <h1>{profileInfo?.age}</h1>
         </div>
-        <p>21m away</p>
-      </div>
-      <div className="w-full flex items-center px-8 gap-4">
-        <div className="flex items-center gap-1">
-          <BsFillPersonFill size={20} />
-          <p>{profileInfo?.gender}</p>
+        <div className="w-full flex items-center gap-4">
+          <div className="flex items-center gap-1">
+            <div className="p-1 bg-green-400 rounded-full" />
+            <p>Online Now</p>
+          </div>
+          <p>21m away</p>
         </div>
-        <p>-</p>
-        <p>{profileInfo?.lookingfor}</p>
-      </div>
-      <div className="w-full flex flex-col px-8 py-8 gap-1">
-        <p>ABOUT ME</p>
-        <div className="bg-gray-400 border rounded-xl p-4 max-w-[620px]">
-          <p>
-            {profileInfo?.bio} 
-          </p>
+        <div className="w-full flex items-center gap-4">
+          <div className="flex items-center gap-1">
+            <BsFillPersonFill size={20} />
+            <p>{profileInfo?.gender}</p>
+          </div>
+          <p>-</p>
+          <p>{profileInfo?.lookingfor}</p>
         </div>
-      </div>
-      <div className="fixed w-full h-screen flex flex-col bottom-12 justify-end items-center">
-        <Message
-          userId={userInfo?.id}
-          username={userInfo?.username}
-          recieverId={profileInfo?.id}
-          recieverName={profileInfo?.username}
-        />
+        <div className="w-full flex flex-col py-8 gap-1">
+          <p className="font-bold">ABOUT ME</p>
+          <div className="bg-gray-400 border rounded-xl p-4 max-w-[620px]">
+            <p>{profileInfo?.bio}</p>
+          </div>
+        </div>
+        <div className="w-full flex flex-col gap-4">
+          <p className="font-bold">STATS</p>
+          <div className="flex items-center gap-4">
+            <BiRuler size={20} />
+            <p>{`6'2"`}</p>
+            <p>{profileInfo?.weight}</p>
+          </div>
+          <div className="flex items-center gap-4">
+            <FaTransgender size={20} />
+            <p>{profileInfo?.gender} - he/him/his</p>
+          </div>
+          <div className="flex items-center gap-4">
+            <PiGlobeStandBold size={20} />
+            <p>{profileInfo?.race}</p>
+          </div>
+          <div className="flex items-center gap-4">
+            <BsPersonHearts size={20} />
+            <p>{profileInfo?.relationshipstatus}</p>
+          </div>
+        </div>
+        <div className="fixed w-full h-screen flex flex-col bottom-12 justify-end items-center">
+          {params.id === userInfo?.id ? (
+            <Link
+              href="/profile/edit"
+              className="bg-yellow-500 px-4 py-2 rounded-full max-w-[380px] opacity-40 hover:opacity-100 ease-in-out duration-200"
+            >
+              <p>Edit Profile</p>
+            </Link>
+          ) : (
+            <Message
+              userId={userInfo?.id}
+              username={userInfo?.username}
+              recieverId={profileInfo?.id}
+              recieverName={profileInfo?.username}
+            />
+          )}
+        </div>
       </div>
     </section>
   );
