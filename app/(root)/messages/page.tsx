83c59bd1cfa9ce796a8ce0541d3de7026592d8b8
Nick <darkforceyoda@gmail.com>
2023-11-01 09:50:33 -0500

    messages to comp

--- app/(root)/messages/page.tsx
+++ app/(root)/messages/page.tsx
@@ -1,12 +1,10 @@
 import LikedBy from "@/components/messages/LikedBy";
+import Messages from "@/components/messages/Messages";
 import NoMessages from "@/components/messages/NoMessages";
 import { populateConversations } from "@/libs/actions/message.actions";
 import { fetchUser, populateLikedBy } from "@/libs/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
-import Image from "next/image";
-import Link from "next/link";
 import React from "react";
-import { BsPersonCircle } from "react-icons/bs";
 
 type pageProps = {};
 
@@ -22,6 +20,7 @@ const page: React.FC<pageProps> = async () => {
     <main className="w-full h-screen overflow-y-scroll scrollbar-hide">
       <h2 className="p-4 font-bold text-3xl">Likes you</h2>
       <LikedBy likedBy={likedBy} />
+
       <h1 className="p-4 font-bold text-3xl">Messages</h1>
       {conversations.length > 0 ? (
         conversations.map((convo) => {
@@ -31,41 +30,13 @@ const page: React.FC<pageProps> = async () => {
               : convo?.users[1];
 
           return (
-            <Link
-              href={`/messages/${convo?._id}`}
-              key={convo.id}
-              className="block border-b p-4 transition-transform transform hover:bg-slate-300 ease-in-out duration-200"
-            >
-              <div className="flex items-center">
-                <div className="w-24 h-24 bg-slate-600 text-gray-200 rounded-full relative flex justify-center items-center">
-                  {otherUser.image ? (
-                    <Image
-                      fill
-                      src={
-                        convo?.users[0].username !== userInfo?.username
-                          ? convo?.users[0].image
-                          : convo?.users[1].image
-                      }
-                      className="object-cover rounded-full"
-                      alt="profile_img"
-                    />
-                  ) : (
-                    <BsPersonCircle size={40} />
-                  )}
-                </div>
-                <div className="ml-4">
-                  <p className="text-lg font-semibold">{otherUser.username}</p>
-                  <p className="text-gray-600">
-                    {convo?.message[convo?.message.length - 1]?.sentBy ===
-                    userInfo?.username
-                      ? "Received: " +
-                        convo?.message[convo?.message.length - 1]?.content
-                      : "Sent: " +
-                        convo?.message[convo?.message.length - 1]?.content}
-                  </p>
-                </div>
-              </div>
-            </Link>
+            <>
+              <Messages
+                convo={convo}
+                otherUser={otherUser}
+                userInfo={userInfo}
+              />
+            </>
           );
         })
       ) : (
---
+++ components/messages/Messages.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { userInfo } from "os";
+import { BsPersonCircle } from "react-icons/bs";
+
+type MessagesProps = {
+  convo: any;
+  otherUser: any;
+  userInfo: any;
+};
+
+const Messages: React.FC<MessagesProps> = ({ convo, otherUser, userInfo }) => {
+  return (
+    <Link
+      href={`/messages/${convo?._id}`}
+      key={convo.id}
+      className="block border-b p-4 transition-transform transform hover:bg-slate-300 ease-in-out duration-200"
+    >
+      <div className="flex items-center">
+        <div className="w-24 h-24 bg-slate-600 text-gray-200 rounded-full relative flex justify-center items-center">
+          {otherUser.image ? (
+            <Image
+              fill
+              src={
+                convo?.users[0].username !== userInfo?.username
+                  ? convo?.users[0].image
+                  : convo?.users[1].image
+              }
+              className="object-cover rounded-full"
+              alt="profile_img"
+            />
+          ) : (
+            <BsPersonCircle size={40} />
+          )}
+        </div>
+        <div className="ml-4">
+          <p className="text-lg font-semibold">{otherUser.username}</p>
+          <p className="text-gray-600">
+            {convo?.message[convo?.message.length - 1]?.sentBy ===
+            userInfo?.username
+              ? "Sent: " + convo?.message[convo?.message.length - 1]?.content
+              : "Received: " +
+                convo?.message[convo?.message.length - 1]?.content}
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+export default Messages;
