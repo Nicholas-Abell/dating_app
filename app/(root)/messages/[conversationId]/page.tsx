6d12dca56e119ca78a63fc6c390a09e3be4f0d84
Nick <darkforceyoda@gmail.com>
2023-11-01 11:08:00 -0500

    add otherUser's image next to text bubble with link to profile

--- app/(root)/messages/[conversationId]/page.tsx
+++ app/(root)/messages/[conversationId]/page.tsx
@@ -1,16 +1,18 @@
-import Message from "@/components/forms/Message";
 import { fetchConversation } from "@/libs/actions/message.actions";
 import { fetchUser } from "@/libs/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 
 async function Page({ params }: { params: { conversationId: string } }) {
   const user = await currentUser();
   if (!user) return null;
-
   const userInfo = await fetchUser(user?.id);
-
-  console.log("params:" + params.conversationId);
   const conversation = await fetchConversation(params?.conversationId);
+  const otherUser = conversation?.users.find(
+    (user: any) => user.username !== userInfo?.username
+  );
+  const otherUserInfo = await fetchUser(otherUser?.id);
 
   return (
     <section className="h-screen w-full gap-4 flex flex-col items-center pt-8 px-12 lg:px-48 overflow-y-scroll scrollbar-hide">
@@ -21,8 +23,21 @@ async function Page({ params }: { params: { conversationId: string } }) {
               {mess.content}
             </div>
           ) : (
-            <div className="border-2 border-white rounded-xl px-12 py-4 bg-green-400 mr-auto">
-              {mess.content}
+            <div className="px-8 relative mr-auto">
+              <div className="border-2 border-white rounded-xl px-12 py-4 bg-green-400 ">
+                {mess.content}
+                <Link
+                  href={`/profile/${otherUser.id}`}
+                  className="absolute bottom-0 left-0 w-6 h-6 rounded-s-full"
+                >
+                  <Image
+                    fill
+                    src={otherUserInfo.images[0]}
+                    alt={otherUser.username}
+                    className="object-fill rounded-full"
+                  />
+                </Link>
+              </div>
             </div>
           )}
         </>
