d9fd34b425234b553c17bc295f17766222e0a884
Nick <darkforceyoda@gmail.com>
2023-11-05 09:24:11 -0600

    loading state for preference page created

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,12 +2,23 @@ import Card from "@/components/cards/Card";
 import { fetchUser, populateUsers } from "@/libs/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 
-export default async function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
   const user = await currentUser();
   if (!user) return null; // to avoid typescript warnings
 
   const userInfo = await fetchUser(user.id);
+
   let profiles = await populateUsers(user.id);
+
+  // const page = searchParams["page" ?? "1"];
+  // const per_page = searchParams["per_page"] ?? "20";
+  // const start = (Number(page) - 1) * Number(per_page);
+  // const end = start + Number(per_page);
+  // const profiles = profilesz.slice(start, end);
 
   const checkLikedProfiles = (userId: string) => {
     if (userInfo?.likes?.includes(userId)) {
---
+++ app/(root)/preferences/loading.tsx
@@ -0,0 +1,13 @@
+import PreferencesSkeleton from "@/components/loadingSkeletons/PreferencesSkeleton";
+import React from "react";
+
+type loadingProps = {};
+
+const loading: React.FC<loadingProps> = () => {
+  return (
+    <>
+      <PreferencesSkeleton />
+    </>
+  );
+};
+export default loading;
