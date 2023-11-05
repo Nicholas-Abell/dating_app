2f222dfda1140bda032057add9918d11272b0250
Nick <darkforceyoda@gmail.com>
2023-11-05 09:23:09 -0600

    style changes to likedBy loading skeleton

--- components/loadingSkeletons/LikedBySkeleton.tsx
+++ components/loadingSkeletons/LikedBySkeleton.tsx
@@ -4,22 +4,7 @@ type LikedBySkeletonProps = {};
 
 const LikedBySkeleton: React.FC<LikedBySkeletonProps> = () => {
   return (
-    <div className="flex items-center gap-8 overflow-x-scroll scrollbar-hide">
-      <div className="w-16 h-16 bg-slate-600 text-gray-200 rounded-full relative flex justify-center items-center">
-        <div className="w-10 h-10 bg-gray-300 rounded-full animate-pulse"></div>
-      </div>
-      <div className="w-16 h-16 bg-slate-600 text-gray-200 rounded-full relative flex justify-center items-center">
-        <div className="w-10 h-10 bg-gray-300 rounded-full animate-pulse"></div>
-      </div>
-      <div className="w-16 h-16 bg-slate-600 text-gray-200 rounded-full relative flex justify-center items-center">
-        <div className="w-10 h-10 bg-gray-300 rounded-full animate-pulse"></div>
-      </div>
-      <div className="w-16 h-16 bg-slate-600 text-gray-200 rounded-full relative flex justify-center items-center">
-        <div className="w-10 h-10 bg-gray-300 rounded-full animate-pulse"></div>
-      </div>
-      <div className="w-16 h-16 bg-slate-600 text-gray-200 rounded-full relative flex justify-center items-center">
-        <div className="w-10 h-10 bg-gray-300 rounded-full animate-pulse"></div>
-      </div>
+    <div className="flex items-center justify-start overflow-x-scroll scrollbar-hide px-4 gap-8">
       <div className="w-16 h-16 bg-slate-600 text-gray-200 rounded-full relative flex justify-center items-center">
         <div className="w-10 h-10 bg-gray-300 rounded-full animate-pulse"></div>
       </div>
