c14abfd94b2229e332a15ce1bc41945f8b0e31d5
Nick <darkforceyoda@gmail.com>
2023-11-05 09:23:40 -0600

    Preference page loading skeleton created

---
+++ components/loadingSkeletons/PreferencesSkeleton.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { MdFamilyRestroom } from "react-icons/md";
+import { PiShapesLight } from "react-icons/pi";
+
+type PreferencesSkeletonProps = {};
+
+const PreferencesSkeleton: React.FC<PreferencesSkeletonProps> = () => {
+  return (
+    <main className="w-full">
+      <div className="bg-gray-200 w-full px-8 py-2 flex items-center gap-4 text-2xl font-bold text-black">
+        <PiShapesLight size={50} />
+        <p>BASIC</p>
+      </div>
+      <div className="w-full px-8 py-4 flex items-center justify-between gap-4 md:text-2xl text-black border-t border-b">
+        <div className="animate-pulse bg-gray-300 h-6 w-32 rounded-lg"></div>
+        <div className="animate-pulse bg-gray-300 h-6 w-6 rounded-full"></div>
+      </div>
+      <div className="w-full px-8 py-4 flex items-center justify-between gap-4 md:text-2xl text-black border-t border-b">
+        <div className="animate-pulse bg-gray-300 h-6 w-32 rounded-lg"></div>
+        <div className="animate-pulse bg-gray-300 h-6 w-6 rounded-full"></div>
+      </div>
+      <div className="w-full px-8 py-4 flex items-center justify-between gap-4 md:text-2xl text-black border-t border-b">
+        <div className="animate-pulse bg-gray-300 h-6 w-32 rounded-lg"></div>
+        <div className="animate-pulse bg-gray-300 h-6 w-6 rounded-full"></div>
+      </div>
+      <div className="w-full px-8 py-4 flex items-center justify-between gap-4 md:text-2xl text-black border-t border-b">
+        <div className="animate-pulse bg-gray-300 h-6 w-32 rounded-lg"></div>
+        <div className="animate-pulse bg-gray-300 h-6 w-6 rounded-full"></div>
+      </div>
+      <div className="bg-gray-200 w-full px-8 py-2 flex items-center gap-4 text-2xl font-bold text-black">
+        <MdFamilyRestroom size={50} />
+        <p>FAMILY</p>
+      </div>
+      <div className="w-full px-8 py-4 flex items-center justify-between gap-4 md:text-2xl text-black border-t border-b">
+        <div className="animate-pulse bg-gray-300 h-6 w-32 rounded-lg"></div>
+        <div className="animate-pulse bg-gray-300 h-6 w-6 rounded-full"></div>
+      </div>
+      <div className="w-full px-8 py-4 flex items-center justify-between gap-4 md:text-2xl text-black border-t border-b">
+        <div className="animate-pulse bg-gray-300 h-6 w-32 rounded-lg"></div>
+        <div className="animate-pulse bg-gray-300 h-6 w-6 rounded-full"></div>
+      </div>
+      <div className="w-full px-8 py-4 flex items-center justify-between gap-4 md:text-2xl text-black border-t border-b">
+        <div className="animate-pulse bg-gray-300 h-6 w-32 rounded-lg"></div>
+        <div className="animate-pulse bg-gray-300 h-6 w-6 rounded-full"></div>
+      </div>
+      <div className="w-full px-8 py-4 flex items-center justify-between gap-4 md:text-2xl text-black border-t border-b">
+        <div className="animate-pulse bg-gray-300 h-6 w-32 rounded-lg"></div>
+        <div className="animate-pulse bg-gray-300 h-6 w-6 rounded-full"></div>
+      </div>
+    </main>
+  );
+};
+export default PreferencesSkeleton;
