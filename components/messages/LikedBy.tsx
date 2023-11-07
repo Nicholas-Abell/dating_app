dc6565d796d145ac4d6ec1596edcd6f7804b9f81
Nick <darkforceyoda@gmail.com>
2023-11-07 10:29:56 -0600

    conditional rendering when no likes by other users

--- components/messages/LikedBy.tsx
+++ components/messages/LikedBy.tsx
@@ -3,31 +3,36 @@ import Image from "next/image";
 import React from "react";
 import User from "@/libs/models/user.model";
 import { findUsersThatLikedYou } from "@/libs/actions/user.actions";
+import NoLikedBy from "./NoLikedBy";
 
 type LikedByProps = {
   userId: string;
 };
 
 const LikedBy: React.FC<LikedByProps> = async ({ userId }) => {
-  const likedz = await findUsersThatLikedYou(userId);
+  const likesYou = await findUsersThatLikedYou(userId);
 
   return (
-    <div className="flex items-center justify-start overflow-x-scroll scrollbar-hide px-4 gap-8">
-      {likedz.map((likedYou: any, key: number) => (
-        <Link
-          key={key}
-          href={`/profile/${likedYou?.id}`}
-          className="w-16 h-16 bg-slate-600 text-gray-200 rounded-full relative flex justify-center items-center"
-        >
-          <Image
-            src={likedYou.images[0]}
-            fill
-            alt={likedYou.username.toString()}
-            className="object-cover rounded-full"
-          />
-        </Link>
-      ))}
-    </div>
+    <aside className="flex items-center justify-start overflow-x-scroll scrollbar-hide px-4 gap-8">
+      {likesYou ? (
+        likesYou.map((likedYou: any, key: number) => (
+          <Link
+            key={key}
+            href={`/profile/${likedYou?.id}`}
+            className="w-16 h-16 bg-slate-600 text-gray-200 rounded-full relative flex justify-center items-center"
+          >
+            <Image
+              src={likedYou.images[0]}
+              fill
+              alt={likedYou.username.toString()}
+              className="object-cover rounded-full"
+            />
+          </Link>
+        ))
+      ) : (
+        <NoLikedBy />
+      )}
+    </aside>
   );
 };
 export default LikedBy;
---
+++ components/messages/NoLikedBy.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type NoLikedByProps = {};
+
+const NoLikedBy: React.FC<NoLikedByProps> = () => {
+  return (
+    <div className="h-16">
+      <p>No likes yet</p>
+    </div>
+  );
+};
+export default NoLikedBy;
