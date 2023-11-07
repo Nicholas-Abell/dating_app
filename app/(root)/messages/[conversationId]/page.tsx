a33469d99f25827127d5ca03a549d6e207267170
Nick <darkforceyoda@gmail.com>
2023-11-06 22:06:13 -0600

    fix: removed fragmet causing key prop error

--- app/(root)/messages/[conversationId]/page.tsx
+++ app/(root)/messages/[conversationId]/page.tsx
@@ -16,34 +16,32 @@ async function Page({ params }: { params: { conversationId: string } }) {
 
   return (
     <section className="h-screen w-full gap-4 flex flex-col items-center pt-8 px-12 lg:px-48 overflow-y-scroll scrollbar-hide">
-      {conversation?.message.map((mess: any) => (
-        <>
-          {mess.sentBy === userInfo?.username ? (
-            <div key={mess._id} className="px-8 relative ml-auto">
-              <div className="border-2 border-black rounded-xl px-12 py-4 bg-blue-300">
-                {mess.content}
-              </div>
+      {conversation?.message.map((mess: any) => {
+        mess.sentBy === userInfo?.username ? (
+          <div key={mess._id} className="px-8 relative ml-auto">
+            <div className="border-2 border-black rounded-xl px-12 py-4 bg-blue-300">
+              {mess.content}
             </div>
-          ) : (
-            <div key={mess._id} className="px-8 relative mr-auto">
-              <div className="border-2 border-black rounded-xl px-12 py-4 bg-green-300">
-                {mess.content}
-                <Link
-                  href={`/profile/${otherUser.id}`}
-                  className="absolute bottom-0 left-0 w-6 h-6 rounded-s-full"
-                >
-                  <Image
-                    fill
-                    src={otherUserInfo.images[0]}
-                    alt={otherUser.username}
-                    className="object-fill rounded-full"
-                  />
-                </Link>
-              </div>
+          </div>
+        ) : (
+          <div key={mess._id} className="px-8 relative mr-auto">
+            <div className="border-2 border-black rounded-xl px-12 py-4 bg-green-300">
+              {mess.content}
+              <Link
+                href={`/profile/${otherUser.id}`}
+                className="absolute bottom-0 left-0 w-6 h-6 rounded-s-full"
+              >
+                <Image
+                  fill
+                  src={otherUserInfo.images[0]}
+                  alt={otherUser.username}
+                  className="object-fill rounded-full"
+                />
+              </Link>
             </div>
-          )}
-        </>
-      ))}
+          </div>
+        );
+      })}
     </section>
   );
 }
