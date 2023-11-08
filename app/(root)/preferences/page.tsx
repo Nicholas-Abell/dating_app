b9f73489b0084c2b968dbbbaa658bb078fbf70ee
Nick <darkforceyoda@gmail.com>
2023-11-08 11:03:56 -0600

    pass user data to preferencesForm

--- app/(root)/preferences/page.tsx
+++ app/(root)/preferences/page.tsx
@@ -1,13 +1,18 @@
-"use client";
 import PreferencesForm from "@/components/forms/PreferencesForm";
+import { fetchUser } from "@/libs/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
 import React from "react";
 
 type pageProps = {};
 
-const page: React.FC<pageProps> = () => {
+const page: React.FC<pageProps> = async () => {
+  const user = await currentUser();
+
+  const userInfo = await fetchUser(user.id);
+
   return (
     <main className="w-full">
-      <PreferencesForm />
+      <PreferencesForm user={userInfo} />
     </main>
   );
 };
