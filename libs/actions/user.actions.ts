5851cffccd1322041ce6988a604aef93d97ac808
Nick <darkforceyoda@gmail.com>
2023-11-11 21:52:42 -0600

    code cleanup

--- libs/actions/user.actions.ts
+++ libs/actions/user.actions.ts
@@ -125,24 +125,6 @@ export async function deleteUserImage(userId: string, imageUrl: string) {
   }
 }
 
-// export async function fetchProfiles(
-//   userId: string,
-//   pageNumber = 1,
-//   pageSize = 20
-// ) {
-//   const skipAmount = (pageNumber - 1) * pageSize;
-
-//   try {
-//     connectToDB();
-//     const users = await User.find({ id: { $ne: userId } })
-//       .skip(skipAmount)
-//       .limit(pageSize);
-//     return users;
-//   } catch (error: any) {
-//     throw new Error("fetchUser Error: ", error);
-//   }
-// }
-
 export async function fetchProfiles(
   userId: string,
   pageNumber = 1,
