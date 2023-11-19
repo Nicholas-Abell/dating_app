8bb073cdaa7529b8d6b816d08bdce706615242b4
Nick <darkforceyoda@gmail.com>
2023-11-18 22:19:05 -0600

    skeleton matches home

--- components/loadingSkeletons/HomeSkeleton.tsx
+++ components/loadingSkeletons/HomeSkeleton.tsx
@@ -6,8 +6,7 @@ type HomeSkeletonProps = {};
 const HomeSkeleton: React.FC<HomeSkeletonProps> = () => {
   return (
     <section className="p-8 w-full">
-      <h1 className="text-center text-4xl py-12">HOME</h1>
-      <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-">
         <CardSkeleton />
         <CardSkeleton />
         <CardSkeleton />
