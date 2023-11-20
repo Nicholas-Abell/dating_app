f7e5a84dca893d39c225dd2667dd5fcc9ac77fce
Nick <darkforceyoda@gmail.com>
2023-11-20 11:18:29 -0600

    updated to match

--- components/loadingSkeletons/PreferencesSkeleton.tsx
+++ components/loadingSkeletons/PreferencesSkeleton.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { BsChevronCompactRight } from "react-icons/bs";
+import { FaGlobeAmericas, FaGlassCheers } from "react-icons/fa";
+import { FaPersonRays } from "react-icons/fa6";
 import { MdFamilyRestroom } from "react-icons/md";
 import { PiShapesLight } from "react-icons/pi";
 
@@ -27,6 +30,7 @@ const PreferencesSkeleton: React.FC<PreferencesSkeletonProps> = () => {
         <div className="animate-pulse bg-gray-300 h-6 w-32 rounded-lg"></div>
         <div className="animate-pulse bg-gray-300 h-6 w-6 rounded-full"></div>
       </div>
+
       <div className="bg-gray-200 w-full px-8 py-2 flex items-center gap-4 text-2xl font-bold text-black">
         <MdFamilyRestroom size={50} />
         <p>FAMILY</p>
@@ -34,6 +38,61 @@ const PreferencesSkeleton: React.FC<PreferencesSkeletonProps> = () => {
       <div className="w-full px-8 py-4 flex items-center justify-between gap-4 md:text-2xl text-black border-t border-b">
         <div className="animate-pulse bg-gray-300 h-6 w-32 rounded-lg"></div>
         <div className="animate-pulse bg-gray-300 h-6 w-6 rounded-full"></div>
+      </div>
+      <div className="w-full px-8 py-4 flex items-center justify-between gap-4 md:text-2xl text-black border-t border-b">
+        <div className="animate-pulse bg-gray-300 h-6 w-32 rounded-lg"></div>
+        <div className="animate-pulse bg-gray-300 h-6 w-6 rounded-full"></div>
+      </div>
+      <div className="w-full px-8 py-4 flex items-center justify-between gap-4 md:text-2xl text-black border-t border-b">
+        <div className="animate-pulse bg-gray-300 h-6 w-32 rounded-lg"></div>
+        <div className="animate-pulse bg-gray-300 h-6 w-6 rounded-full"></div>
+      </div>
+
+      <div className="bg-gray-200 w-full px-8 py-2 flex items-center gap-4 text-2xl font-bold text-black">
+        <FaPersonRays size={50} />
+        <p>LOOKS</p>
+      </div>
+      <div className="w-full px-8 py-4 flex items-center justify-between gap-4 md:text-2xl text-black border-t border-b">
+        <div className="animate-pulse bg-gray-300 h-6 w-32 rounded-lg"></div>
+        <div className="animate-pulse bg-gray-300 h-6 w-6 rounded-full"></div>
+      </div>
+      <div className="w-full px-8 py-4 flex items-center justify-between gap-4 md:text-2xl text-black border-t border-b">
+        <div className="animate-pulse bg-gray-300 h-6 w-32 rounded-lg"></div>
+        <div className="animate-pulse bg-gray-300 h-6 w-6 rounded-full"></div>
+      </div>
+
+      <div className="bg-gray-200 w-full px-8 py-2 flex items-center gap-4 text-2xl font-bold text-black">
+        <FaGlobeAmericas size={50} />
+        <p>BACKGROUND</p>
+      </div>
+      <div className="w-full px-8 py-4 flex items-center justify-between gap-4 md:text-2xl text-black border-t border-b">
+        <div className="animate-pulse bg-gray-300 h-6 w-32 rounded-lg"></div>
+        <div className="animate-pulse bg-gray-300 h-6 w-6 rounded-full"></div>
+      </div>
+      <div className="w-full px-8 py-4 flex items-center justify-between gap-4 md:text-2xl text-black border-t border-b">
+        <div className="animate-pulse bg-gray-300 h-6 w-32 rounded-lg"></div>
+        <div className="animate-pulse bg-gray-300 h-6 w-6 rounded-full"></div>
+      </div>
+      <div className="w-full px-8 py-4 flex items-center justify-between gap-4 md:text-2xl text-black border-t border-b">
+        <div className="animate-pulse bg-gray-300 h-6 w-32 rounded-lg"></div>
+        <div className="animate-pulse bg-gray-300 h-6 w-6 rounded-full"></div>
+      </div>
+      <div className="w-full px-8 py-4 flex items-center justify-between gap-4 md:text-2xl text-black border-t border-b">
+        <div className="animate-pulse bg-gray-300 h-6 w-32 rounded-lg"></div>
+        <div className="animate-pulse bg-gray-300 h-6 w-6 rounded-full"></div>
+      </div>
+      <div className="w-full px-8 py-4 flex items-center justify-between gap-4 md:text-2xl text-black border-t border-b">
+        <div className="animate-pulse bg-gray-300 h-6 w-32 rounded-lg"></div>
+        <div className="animate-pulse bg-gray-300 h-6 w-6 rounded-full"></div>
+      </div>
+      <div className="w-full px-8 py-4 flex items-center justify-between gap-4 md:text-2xl text-black border-t border-b">
+        <div className="animate-pulse bg-gray-300 h-6 w-32 rounded-lg"></div>
+        <div className="animate-pulse bg-gray-300 h-6 w-6 rounded-full"></div>
+      </div>
+
+      <div className="bg-gray-200 w-full px-8 py-2 flex items-center gap-4 text-2xl font-bold text-black">
+        <FaGlassCheers size={50} />
+        <p>LIFESTYLE</p>
       </div>
       <div className="w-full px-8 py-4 flex items-center justify-between gap-4 md:text-2xl text-black border-t border-b">
         <div className="animate-pulse bg-gray-300 h-6 w-32 rounded-lg"></div>
