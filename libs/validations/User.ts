ae00637078aace22b4b4ef5820de0066c3d3e24e
Nick <darkforceyoda@gmail.com>
2023-11-22 10:22:33 -0600

    updated validations for new user model and preferences

--- libs/validations/User.ts
+++ libs/validations/User.ts
@@ -11,6 +11,14 @@ export const UserValidation = z.object({
   sexualOrientation: z.string(),
   gender: z.string(),
   race: z.string(),
+  pronouns: z.string(),
+  pets: z.string(),
+  kids: z.string(),
+  alcohol: z.string(),
+  smoking: z.string(),
+  marijuana: z.string(),
+  religion: z.string(),
+  politicalViews: z.string(),
 });
 
 export const LocationValidation = z.object({
@@ -24,4 +32,14 @@ export const PreferencesValidation = z.object({
   age: z.object({ min: z.number().min(18), max: z.number().min(18) }),
   distance: z.number().min(50),
   desires: z.array(z.string()),
+  relationshipstatus: z.array(z.string()),
+  pets: z.array(z.string()),
+  kids: z.array(z.string()),
+  alcohol: z.array(z.string()),
+  smoking: z.array(z.string()),
+  marijuana: z.array(z.string()),
+  religion: z.array(z.string()),
+  politicalViews: z.array(z.string()),
+  race: z.array(z.string()),
+  sexualOrientation: z.array(z.string()),
 });
