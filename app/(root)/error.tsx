dc724922c5dc59f8c4d44ab50ccc8c0d8ac29b10
Nick <darkforceyoda@gmail.com>
2023-11-26 22:46:47 -0600

    error page

---
+++ app/(root)/error.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FaHeartBroken } from "react-icons/fa";
+
+type errorProps = {};
+
+const error: React.FC<errorProps> = () => {
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center text-gray-300 gap-4">
+      <FaHeartBroken size={60} />
+      <p>Oops! Something went wrong. Please try again later.</p>
+    </div>
+  );
+};
+export default error;
