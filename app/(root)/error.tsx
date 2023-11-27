47b2517f6bb4264173a7161aa03b703b81ca7037
Nick <darkforceyoda@gmail.com>
2023-11-27 09:58:34 -0600

    error text color

--- app/(root)/error.tsx
+++ app/(root)/error.tsx
@@ -6,7 +6,7 @@ type errorProps = {};
 
 const error: React.FC<errorProps> = () => {
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center text-gray-300 gap-4">
+    <div className="w-full h-screen flex flex-col justify-center items-center text-gray-400 gap-4">
       <FaHeartBroken size={60} />
       <p>Oops! Something went wrong. Please try again later.</p>
     </div>
