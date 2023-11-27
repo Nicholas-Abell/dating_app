82a44b8972c5e97d877b02b411e7bef78ca6fdb6
Nick <darkforceyoda@gmail.com>
2023-11-27 10:12:29 -0600

    error page mobile styling

--- app/(root)/error.tsx
+++ app/(root)/error.tsx
@@ -8,7 +8,10 @@ const error: React.FC<errorProps> = () => {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center text-gray-400 gap-4">
       <FaHeartBroken size={60} />
-      <p>Oops! Something went wrong. Please try again later.</p>
+      <p>
+        Oops! Something went wrong. <br className="md:hidden" />
+        Please try again later.
+      </p>
     </div>
   );
 };
